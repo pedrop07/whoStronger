7bbea092af3ab9c884f99da6125c76de61cd5f4d
Pedro <pedrofulfaro@gmail.com>
2023-10-30 11:45:55 -0300

    feat: add server actions

---
+++ src/actions/actions.ts
@@ -0,0 +1,44 @@
+'use server';
+
+import { Hero } from '../types';
+
+interface GetHeroesParams {
+  filterByName?: string;
+  filterByGender?: string;
+  filterByAlignment?: string;
+}
+
+const apiURL = process.env.API_URL;
+
+export async function getHeroes({
+  filterByName = '',
+  filterByGender = '',
+  filterByAlignment = '',
+}: GetHeroesParams): Promise<Hero[]> {
+  if (!apiURL) {
+    console.error(
+      'A variável apiURL não está definida. Certifique-se de configurá-la corretamente.',
+    );
+    return [];
+  }
+
+  const res = await fetch(apiURL);
+
+  if (res.ok) {
+    const heroes: Hero[] = await res.json();
+
+    if (filterByName || filterByGender || filterByAlignment) {
+      return heroes.filter((hero) => {
+        return (
+          hero.name.toLowerCase().includes(filterByName.toLowerCase()) &&
+          hero.appearance.gender.includes(filterByGender) &&
+          hero.biography.alignment.includes(filterByAlignment)
+        );
+      });
+    }
+
+    return heroes;
+  }
+
+  return [];
+}
